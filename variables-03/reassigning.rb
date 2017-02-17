91b06b38111be04d10c3b93dbc95b6aaa9bc5eb1
Timothy Robinson <timmyrobby@gmail.com>
2017-02-17 12:16:27 +0000

    Reassigning variables

---
+++ variables-03/reassigning.rb
@@ -0,0 +1,12 @@
+composer = 'Mozart'
+puts composer + ' was "da bomb", in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+
+
+var = 'just another ' + 'string'
+puts var
+
+var = 5 * (1+2)
+puts var
