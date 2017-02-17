a05c3ea989edb9118f6fe6a233cf2e06692d52de
Timothy Robinson <timmyrobby@gmail.com>
2017-02-17 15:10:32 +0000

    Powers, square roots and modulus

---
+++ chap-05-methods/arithmetic.rb
@@ -0,0 +1,19 @@
+# Five squared
+puts 5**2
+
+# Square root of Five
+puts 5**0.5
+
+# Seven divided by 3, without remainder
+puts 7/3
+
+# Using a modulus to give the remainder of the division
+puts 7%3
+
+# Ditto, but weeks into a year
+puts 365%7
+
+
+# Using 'abs' finds the absolute value of the number.  Both produce '3'.
+puts (5-2).abs
+puts (2-5).abs
