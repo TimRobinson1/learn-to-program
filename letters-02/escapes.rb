affe35524597b00fc728ad74f272d2f86e4034e8
Timothy Robinson <timmyrobby@gmail.com>
2017-02-17 12:04:51 +0000

    Escape characters

---
+++ letters-02/escapes.rb
@@ -0,0 +1,7 @@
+# This is about escape characters, which allows the use of special characters
+# That would otherwise ruin the code
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
