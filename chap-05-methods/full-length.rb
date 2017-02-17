a2eb050577436fde089a7d58fca8b2d8cdc55257
Timothy Robinson <timmyrobby@gmail.com>
2017-02-17 14:25:05 +0000

    Counts the full name, no spaces

---
+++ chap-05-methods/full-length.rb
@@ -0,0 +1,9 @@
+puts "Please enter your first name."
+f_name = gets.chomp
+puts "Please enter your middle name."
+m_name = gets.chomp
+puts "Please enter your last name."
+l_name = gets.chomp
+name = f_name + m_name + l_name
+
+puts "Did you know that there are #{name.length} letters in your name?"
