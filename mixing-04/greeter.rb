707a20531e61af7ad44cb372e5a2149261c7b9f6
Timothy Robinson <timmyrobby@gmail.com>
2017-02-17 14:14:51 +0000

    Removing duplicate folder

--- mixing-04/greeter.rb
+++
@@ -1,7 +0,0 @@
-puts "Hello, what is your first name?"
-first_name = gets.chomp
-puts "Wonderful, hello #{first_name}.  What is your middle name?"
-middle_name = gets.chomp
-puts "It's #{middle_name}?  Great.  And your last name?"
-last_name = gets.chomp
-puts "Fantastic, so your name is #{first_name} #{middle_name} #{last_name}.  Welcome!"
