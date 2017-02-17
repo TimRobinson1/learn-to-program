d7eeda1dc8b92fdbb8e6e77ca3b48a9db8b66da6
Timothy Robinson <timmyrobby@gmail.com>
2017-02-17 13:51:33 +0000

    Final suggested program from tutorial

---
+++ mixing-04/favourite-number.rb
@@ -0,0 +1,5 @@
+puts "What is your favourite number?"
+number = gets.chomp
+better_number = number.to_i + 1
+puts "Have you considered #{better_number} as your favourite number instead?"
+puts "It's bigger and, well, by that metric, better."
