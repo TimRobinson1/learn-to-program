1f036ed43a0faa4d05d04059b7eeb7c385f3e845
Timothy Robinson <timmyrobby@gmail.com>
2017-02-17 15:20:28 +0000

    Using srand to seed numbers

--- chap-05-methods/random.rb
+++ chap-05-methods/random.rb
@@ -11,3 +11,18 @@ puts rand(9999999999999999999)
 
 puts "The weatherman said there is a #{rand(101)}% chance of rain,"
 puts "but you can never trust a weatherman."
+puts ''
+
+srand 1776
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts ''
+srand 1776
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
