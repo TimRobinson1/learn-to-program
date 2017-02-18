a05afae44bbb1defb5f3e03df88fa60d85daba64
Timothy Robinson <timmyrobby@gmail.com>
2017-02-18 12:07:31 +0000

    Logic statments

--- chap-06-flow/logic.rb
+++ chap-06-flow/logic.rb
@@ -1,6 +1,18 @@
-puts "Hello, what's your name?"
-name = gets.chomp
-puts "Hello, " + name + "."
-if (name == "Chris" or name == "Katy")
-  puts "What a lovely name!"
-end
+iAmChris = true
+iAmPurple = false
+iLikeFood = true
+iEatRocks = false
+
+# && can be used for and, || can be used for or.
+puts (iAmChris and iLikeFood)
+puts (iLikeFood and iEatRocks)
+puts (iAmPurple && iLikeFood)
+puts (iAmPurple && iEatRocks)
+puts ""
+puts (iAmChris or iLikeFood)
+puts (iLikeFood or iEatRocks)
+puts (iAmPurple || iLikeFood)
+puts (iAmPurple || iEatRocks)
+puts ""
+puts (not iAmPurple)
+puts (not iAmChris)
