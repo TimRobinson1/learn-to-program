cf7a1b84b1fa5d97a84bedab728709b1d3dd4ebf
Timothy Robinson <timmyrobby@gmail.com>
2017-02-18 12:15:30 +0000

    Beer bottle song

---
+++ chap-06-flow/beersong.rb
@@ -0,0 +1,9 @@
+number = 100
+while number != 'No'
+  number -= 1
+  if number == 0
+    number = 'No'
+  end
+    puts "#{number} bottles of beer on the wall!"
+    puts "Take them down, pass them around... #{number} bottles of beer on the wall!"
+end
