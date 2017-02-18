8904428798cda253146b300e28236c33b66a1bc6
Timothy Robinson <timmyrobby@gmail.com>
2017-02-18 11:52:27 +0000

    Embedded if statements

---
+++ chap-06-flow/if-statements.rb
@@ -0,0 +1,16 @@
+puts "Hello, and welcome to 7th grade English."
+puts "My name is Mrs. Gabbard.  And your name is...?"
+
+if name == name.capitalize
+  puts "Please take a seat, " + name + "."
+else
+  puts name + "?  You mean " + name.capitalize + ", right?"
+  puts "Don't you even know how to spell your own name?"
+  reply = gets.chomp
+
+  if reply.downcase == 'yes'
+    puts "Hmmph!  Well, sit down!"
+  else
+    puts "Get out!"
+  end
+end
