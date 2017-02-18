5124fc7b5bde2156ba4e2c866999361f5bcb319b
Timothy Robinson <timmyrobby@gmail.com>
2017-02-18 11:43:07 +0000

    Basic comparison methods

--- chap-06-flow/comparisons.rb
+++ chap-06-flow/comparisons.rb
@@ -1,2 +1,12 @@
+# Returns true or false
 puts 1 > 2
 puts 1 < 2
+
+puts 5 >= 5
+puts 5 <= 4
+
+puts 1 == 1
+puts 2 != 1
+
+# Strings are compared by their lexographical order, so cat comes alphabetically before dog.
+puts 'cat' < 'dog'
