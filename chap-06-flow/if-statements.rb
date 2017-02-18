5e2ad5487d953f8a767cb2a902c23b513aac3988
Timothy Robinson <timmyrobby@gmail.com>
2017-02-18 11:54:01 +0000

    Added missing variable

--- chap-06-flow/if-statements.rb
+++ chap-06-flow/if-statements.rb
@@ -1,5 +1,6 @@
 puts "Hello, and welcome to 7th grade English."
 puts "My name is Mrs. Gabbard.  And your name is...?"
+name = gets.chomp
 
 if name == name.capitalize
   puts "Please take a seat, " + name + "."
