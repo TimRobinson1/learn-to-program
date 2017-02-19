ad4c8d0ec06313680cf4c8ca99d2e9cd257e9c36
Timothy Robinson <timmyrobby@gmail.com>
2017-02-19 09:44:34 +0000

    Added repeating "bye" function

--- chap-06-flow/deaf-grandma.rb
+++ chap-06-flow/deaf-grandma.rb
@@ -1,12 +1,19 @@
 puts "What is it, sonny?"
-while true
+bye = 0
+while bye != 3
   reply = gets.chomp
   if reply == "BYE"
-    exit(0)
+    bye += 1
   end
-  if reply == reply.upcase
+  if (reply == reply.upcase && reply != "BYE")
     puts "NO, NOT SINCE 19#{rand(30..50)}!"
+    bye = 0
+  elsif reply == "BYE"
+    puts "WHAT'S THAT YOU SAY?"
   else
     puts "HUH?!  SPEAK UP, SONNY!"
+    bye = 0
   end
 end
+
+puts "OH OKAY, BYE DEAR!"
