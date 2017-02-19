96ceea59ca8578af661967f047ebfa9f26036fa7
Timothy Robinson <timmyrobby@gmail.com>
2017-02-19 10:20:08 +0000

    Leap year counting program

--- chap-06-flow/leapyears.rb
+++ chap-06-flow/leapyears.rb
@@ -4,7 +4,17 @@ puts "Thanks. End year, please."
 final = gets.chomp
 
 array = (start..final).to_a
+leap = 0
 
 # At this stage, program only takes the input and makes it an array (to be sorted through later)
 
-puts array
+array.each do |x|
+  y = x.to_i
+  if (y % 4 == 0) && (y % 100 != 0)
+    leap += 1
+  elsif (y % 400 == 0)
+    leap += 1
+  end
+end
+
+puts "There are #{leap} leap years between #{start} and #{final}."
