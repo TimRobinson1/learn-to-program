feac32bf4bda1d01aba26f60b029338800f84084
Timothy Robinson <timmyrobby@gmail.com>
2017-02-19 10:35:12 +0000

    Added in list of leap years

--- chap-06-flow/leapyears.rb
+++ chap-06-flow/leapyears.rb
@@ -5,17 +5,18 @@ final = gets.chomp
 
 # Establishing array and integer
 array = (start..final).to_a
-leap = 0
+leap = []
 
 # Looping through each year in array.
 
 array.each do |i|
   x = i.to_i
   if (x % 4 == 0) && (x % 100 != 0)
-    leap += 1
+    leap.push(x)
   elsif (x % 400 == 0)
-    leap += 1
+    leap.push(x)
   end
 end
 
-puts "There are #{leap} leap years between #{start} and #{final}."
+puts "There are #{leap.count} leap years between #{start} and #{final}."
+puts "These leap years are #{leap.join(', ')}."
