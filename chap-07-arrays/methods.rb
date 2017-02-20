4153d9eda2005a97ec5f1d0b9b93390263a9396f
Timothy Robinson <timmyrobby@gmail.com>
2017-02-20 16:49:48 +0000

    Using pop, push and last

--- chap-07-arrays/methods.rb
+++ chap-07-arrays/methods.rb
@@ -11,3 +11,15 @@ puts foods.join(" :) ") + "  8)"
 200.times do
   puts [[], []]
 end
+
+favorites = []
+favorites.push "raindrops on roses"
+favorites.push "whiskers on kittens"
+
+puts favorites[0]
+puts favorites.last
+puts favorites.length
+
+puts favorites.pop
+puts favorites
+puts favorites.length
