4dbc4987dda433f2ef565cda1cb3e8915b74df58
Timothy Robinson <timmyrobby@gmail.com>
2017-02-20 17:07:16 +0000

    Refactored input into while loop

--- chap-07-arrays/alphabetiser.rb
+++ chap-07-arrays/alphabetiser.rb
@@ -1,11 +1,9 @@
 array = []
 puts "Please enter a word!"
-input = gets.chomp
 
-while input != ""
+while (input = gets.chomp) != ""
   array.push(input)
   puts "Great, and another!"
-  input = gets.chomp
 end
 
 puts "Alphabetising.... Done!"
