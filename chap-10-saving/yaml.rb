c13fd8ab921091c8acc73a8b86b8d16d8d991a62
Timothy Robinson <timmyrobby@gmail.com>
2017-02-24 16:52:51 +0000

    Added text file and changed file location

--- chap-10-saving/yaml.rb
+++ chap-10-saving/yaml.rb
@@ -4,7 +4,7 @@ test_array = ["Give Quiche A Chance", "Mutants Out!", "Chameleonic Life-Forms, N
 
 test_string = test_array.to_yaml
 
-filename = "RimmerTShirts.txt"
+filename = "./chap-10-saving/RimmerTShirts.txt"
 
 File.open filename, 'w' do |f|
   f.write test_string
@@ -14,5 +14,6 @@ read_string = File.read filename
 
 read_array = YAML::load read_string
 
+# These verify that what is saved is correct.
 puts(read_string == test_string)
 puts(read_array == test_array)
