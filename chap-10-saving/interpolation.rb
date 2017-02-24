dd34b600e23a7076e1e3e3b2e5233fb3e7f5a9da
Timothy Robinson <timmyrobby@gmail.com>
2017-02-24 17:04:36 +0000

    Maths inside a string with interpolation

--- chap-10-saving/interpolation.rb
+++ chap-10-saving/interpolation.rb
@@ -3,3 +3,6 @@ zip = 90210
 
 # Using #{} around variables etc. in a string calculates them correctly.
 puts "Name: #{name}, Zipcode: #{zip}"
+
+# Maths can be performed with interpolation.
+puts "#{2 * 10**4} Leagues Under the Sea!"
