0988a8454e540f2743f27a8268b256abd620b694
Timothy Robinson <timmyrobby@gmail.com>
2017-02-24 16:58:41 +0000

    Demonstrating the difference between double and single quotes

---
+++ chap-10-saving/string-quotes.rb
@@ -0,0 +1,11 @@
+buffy_q_1 = '\'Kiss rocks\'?
+            Why would anyone want to kiss...
+            Oh, wait.  I get it.'
+
+buffy_q_2 = "'Kiss rocks'\n" +
+            "Why would anyone want to kiss...\n" +
+            "Oh, wait.  I get it."
+
+puts buffy_q_1
+puts
+puts(buffy_q_1 == buffy_q_2)
