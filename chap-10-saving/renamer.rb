1e8f7992ead2ba6a7e146c18f70913ae84372391
Timothy Robinson <timmyrobby@gmail.com>
2017-02-25 21:36:00 +0000

    Example file renamer

---
+++ chap-10-saving/renamer.rb
@@ -0,0 +1,28 @@
+Dir.chir 'C:/Documents and Settings/Katy/PictureInbox'
+
+pic_names = Dir['F:/**/*.jpg']
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts ""
+print "Downloading #{pic_names.length} files:  "
+
+pic_number = 1
+
+pic_names.each do |names|
+  print '.' # Progress bar
+
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+
+  File.rename name, new_name
+
+  pic_number = pic_number + 1
+end
+
+puts "" # To avoid progress bar line.
+puts "Done!"
