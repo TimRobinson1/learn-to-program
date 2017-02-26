f7916819df323f3fb503b40493d595a09ee5775e
Timothy Robinson <timmyrobby@gmail.com>
2017-02-26 23:35:31 +0000

    Using a range with a string

--- chap-11-classes/strings.rb
+++ chap-11-classes/strings.rb
@@ -3,16 +3,6 @@ lord_voldemort = "Tom Riddle"
 initials = lord_voldemort[0] + "." + lord_voldemort[4] + "."
 puts initials
 
-puts "Hello.  My name is Julian."
-puts "I'm extremely perceptive."
-puts "What's your name?"
-
-name = gets.chomp
-puts "Hi, #{name}."
-
-if name[0] == "D"
-  puts "You have excellent taste in clothing."
-  puts "I can just tell."
-else
-  puts "You are pretty average."
-end
+prof = "We tore the universe a new space-hole, alright!"
+puts prof[12, 8]
+puts prof[12..19] # 8 characters total
