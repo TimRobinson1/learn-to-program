006f69353c6c3c581945662b0171fc9449f95545
Timothy Robinson <timmyrobby@gmail.com>
2017-02-26 23:20:21 +0000

    Iterating through ranges

--- chap-11-classes/ranges.rb
+++ chap-11-classes/ranges.rb
@@ -8,3 +8,18 @@ puts(["a", "b", "c", "d"] == letters.to_a)
 # Array with integers
 numbers = 1..10
 puts numbers.to_a
+
+# Iterating over a range
+("A".."Z").each do |letter|
+  print letter
+end
+
+puts ""
+
+the_70s = 1970..1979
+
+puts the_70s.min
+puts the_70s.max
+puts the_70s.include?(1979)
+puts the_70s.include?(1980)
+puts the_70s.include?(1974.5)
