5c0f77a9e5a7c88fef02e543a3a5928606364be5
Timothy Robinson <timmyrobby@gmail.com>
2017-02-26 23:30:52 +0000

    Example usage of "[...]" method

--- chap-11-classes/strings.rb
+++ chap-11-classes/strings.rb
@@ -2,3 +2,17 @@
 lord_voldemort = "Tom Riddle"
 initials = lord_voldemort[0] + "." + lord_voldemort[4] + "."
 puts initials
+
+puts "Hello.  My name is Julian."
+puts "I'm extremely perceptive."
+puts "What's your name?"
+
+name = gets.chomp
+puts "Hi, #{name}."
+
+if name[0] == "D"
+  puts "You have excellent taste in clothing."
+  puts "I can just tell."
+else
+  puts "You are pretty average."
+end
