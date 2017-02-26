7fa58ab2a6b6b794a336d0afccd52e197c10c47b
Timothy Robinson <timmyrobby@gmail.com>
2017-02-26 23:17:13 +0000

    Basic integer range added

--- chap-11-classes/ranges.rb
+++ chap-11-classes/ranges.rb
@@ -1,5 +1,10 @@
 # Basic array
-
-letters = "a".."l"
-
+letters = "a".."d"
 puts letters.to_a
+
+# Verify this has worked as expected:
+puts(["a", "b", "c", "d"] == letters.to_a)
+
+# Array with integers
+numbers = 1..10
+puts numbers.to_a
