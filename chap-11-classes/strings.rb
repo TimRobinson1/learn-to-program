e30f0686e45510a4d8a1054c0eef62f83de98a3d
Timothy Robinson <timmyrobby@gmail.com>
2017-02-26 23:38:48 +0000

    Using negative range for new method

--- chap-11-classes/strings.rb
+++ chap-11-classes/strings.rb
@@ -6,3 +6,11 @@ puts initials
 prof = "We tore the universe a new space-hole, alright!"
 puts prof[12, 8]
 puts prof[12..19] # 8 characters total
+puts ""
+
+def is_avi? filename
+  filename.downcase[-4..-1] == ".avi"
+end
+
+puts is_avi?("test-video.avi")
+puts is_avi?("test-image.png")
