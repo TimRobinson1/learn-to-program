d2bbc3bfc2de7033ecf5e86b68ec7e0425ff6ee0
Timothy Robinson <timmyrobby@gmail.com>
2017-02-27 01:43:22 +0000

    Refactored and commented

--- chap-11-classes/birthday.rb
+++ chap-11-classes/birthday.rb
@@ -8,6 +8,7 @@ end
 
 filename = "./chap-11-classes/Birthdays.txt"
 
+# Hash for converting the month into an integer
 months = {"Jan" => "01", "Feb" => "02", "Mar" => "03", "Apr" => "04",
   "May" => "05", "Jun" => "06", "Jul" => "07", "Aug" => "08",
   "Sep" => "09", "Oct" => "10", "Nov" => "11", "Dec" => "12"}
@@ -18,16 +19,24 @@ read_array = yaml_load filename
 puts "Who's birthday would you like to check?"
 input = gets.chomp
 
+# Birth date read from file
 date = read_array[input][-12..-1]
-m = months[date[0..2]]
+
+# Birth date broken down for ease into day, month and year - d, m, y
+m = months[date[0..2]].to_i
 d = date[4..5]
 y = date[-4..-1]
-birth = Time.new(y, m, d)
+
+# Current time, then broken into current day and month
 now = Time.now
 current_month = now.to_s[5..6].to_i
 current_day = now.to_s[8..9].to_i
+
+# String representing when their next birthday is.
 is_next_year = "#{input}'s next birthday is: #{date[0..6]} 2018"
 is_this_year = "#{input}'s birthday is this year, on #{date[0..5]}."
+
+# Ages for this year, next year and the current day.
 age = "He will be #{2018 - y.to_i} years old."
 age_soon = "He will be #{2017 - y.to_i} years old."
 age_now = "He is #{2018 - y.to_i} years old."
