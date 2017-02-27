11ad0c5394ce53fdb88049fe556638c521befe7a
Timothy Robinson <timmyrobby@gmail.com>
2017-02-27 00:10:14 +0000

    Removed hard-coded hash

--- chap-11-classes/birthday.rb
+++ chap-11-classes/birthday.rb
@@ -1,27 +1,14 @@
 require "yaml"
 
-def yaml_save object, filename
-  File.open filename, 'w' do |f|
-    f.write(object.to_yaml)
-  end
-end
-
 def yaml_load filename
   yaml_string = File.read filename
 
   YAML::load yaml_string
 end
 
-variable = "test variable"
-
-test_hash = {"Dave Gorman" => "2nd March, 1971"}
-
 filename = "./chap-11-classes/Birthdays.txt"
-
-# Saving
-yaml_save test_hash, filename
 
 # Loading
 read_array = yaml_load filename
 
-puts read_array["Dave Gorman"]
+puts read_array["Dave Franco"]
