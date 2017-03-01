56fe745f1fce336ac5ac279cfc46aa8c25d28616
Timothy Robinson <timmyrobby@gmail.com>
2017-03-01 12:36:49 +0000

    Old style roman numerals, works up to 49

--- chap-08-writing-methods/roman-numerals.rb
+++ chap-08-writing-methods/roman-numerals.rb
@@ -1,9 +1,17 @@
+# This program produces old-style roman numerals
+# i.e. 9 is VIIII rather than IX
+
 def romanise number
-  fives = number / 5
-  answer = "V"*fives
-  remainder = number - fives*5
-  answer += "I"*remainder
+  times = number / 10
+  answer = "X"*times
+  number = number - times*10
+
+  times = number / 5
+  answer += "V"*times
+  number = number - times*5
+
+  answer += "I"*number
   puts answer
 end
 
-romanise(7)
+romanise(18)
