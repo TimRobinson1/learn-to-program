1cf61efb49d1266908b7207b3a57d598df28e16a
Timothy Robinson <timmyrobby@gmail.com>
2017-03-01 13:15:09 +0000

    Modern roman numerals, up to "X"

--- chap-08-writing-methods/modern-roman.rb
+++ chap-08-writing-methods/modern-roman.rb
@@ -1,9 +1,19 @@
 def romanise number
 
+  # For tens, or 'X'
+  times = number / 10
+  answer = "X"*times
+  number = number - times*10
+
   # For fives, or 'V'
-  times = number / 5
-  answer = "V"*times
-  number = number - times*5
+  if number % 5 == 4 && number != 4
+    answer += "IX"
+    number = number - 9
+  else
+    times = number / 5
+    answer += "V"*times
+    number = number - times*5
+  end
 
   if (number % 4 == 0) && (number != 0)
     answer += "IV"
@@ -23,3 +33,15 @@ romanise(5)
 romanise(6)
 romanise(7)
 romanise(8)
+romanise(9)
+romanise(10)
+romanise(11)
+romanise(12)
+romanise(13)
+romanise(14)
+romanise(15)
+romanise(16)
+romanise(17)
+romanise(18)
+romanise(19)
+romanise(20)
