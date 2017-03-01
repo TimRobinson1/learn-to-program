d5b6971cac62f1fc8b016f4a5322262b225d6967
Timothy Robinson <timmyrobby@gmail.com>
2017-03-01 12:39:05 +0000

    Old style roman numerals, up to 99

--- chap-08-writing-methods/roman-numerals.rb
+++ chap-08-writing-methods/roman-numerals.rb
@@ -2,10 +2,17 @@
 # i.e. 9 is VIIII rather than IX
 
 def romanise number
+  # For fifties, or 'L'
+  times = number / 50
+  answer = "L"*times
+  number = number - times*50
+
+  # For tens, or 'X'
   times = number / 10
-  answer = "X"*times
+  answer += "X"*times
   number = number - times*10
 
+  # For fives, or 'V'
   times = number / 5
   answer += "V"*times
   number = number - times*5
@@ -14,4 +21,4 @@ def romanise number
   puts answer
 end
 
-romanise(18)
+romanise(68)
