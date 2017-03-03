7b9c299c2a2835eaee0d54278fe610b1a69a9fd4
Timothy Robinson <timmyrobby@gmail.com>
2017-03-03 13:02:14 +0000

    Sorting a string and array alphabetically

--- chap-09-review/sorting.rb
+++ chap-09-review/sorting.rb
@@ -1,13 +1,28 @@
 list = ["cats", "dogs", "frogs", "elves", "animals", "boys", "girls"]
+string = "olives and monkeys are orgaised according to xenophobic ideals in categorization of forms of life"
+str = string.split(" ")
 
-sorted = []
+sorted_array = []
+sorted_string = []
 
 until (list == []) do
   word = list.min
 
   list.delete(word)
 
-  sorted.push(word)
+  sorted_array.push(word)
 end
 
-puts sorted
+until (str == []) do
+  word = str.min
+
+  str.delete(word)
+
+  sorted_string.push(word)
+end
+
+puts "Alphabetized, the array is:"
+puts sorted_array
+puts ""
+puts "Alphabetized, the string is:"
+puts sorted_string.join(" ")
