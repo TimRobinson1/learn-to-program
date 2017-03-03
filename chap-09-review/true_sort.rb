6fc06b2845277460190698d747e3d088181d5c0f
Timothy Robinson <timmyrobby@gmail.com>
2017-03-03 14:02:30 +0000

    Converted to Array class method

--- chap-09-review/true_sort.rb
+++ chap-09-review/true_sort.rb
@@ -1,25 +1,34 @@
+class Array
+
+  def dictionary_sort
+
+    list = self.join(" ").downcase.split(" ")
+
+    mid_array = []
+
+    sorted = []
+
+    until (list == []) do
+      word = list.min
+
+      list.delete(word)
+
+      mid_array.push(word)
+    end
+
+    mid_array.each do |x|
+      if self.include?(x)
+        sorted.push(x)
+      else
+        sorted.push(x.capitalize)
+      end
+    end
+
+    sorted
+  end
+
+end
+
 arr = ["Apples", "bananas", "Grapes", "cherries", "Delicious", "exciting", "fruit"]
 
-list = arr.join(" ").downcase.split(" ")
-
-mid_array = []
-
-sorted = []
-
-until (list == []) do
-  word = list.min
-
-  list.delete(word)
-
-  mid_array.push(word)
-end
-
-mid_array.each do |x|
-  if arr.include?(x)
-    sorted.push(x)
-  else
-    sorted.push(x.capitalize)
-  end
-end
-
-puts sorted
+puts arr.dictionary_sort
