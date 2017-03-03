21d5ece38a84c2525da8d1dad4faeea5aabb9d90
Timothy Robinson <timmyrobby@gmail.com>
2017-03-03 14:38:14 +0000

    Converted into Integer method

--- chap-08-writing-methods/modern-roman.rb
+++ chap-08-writing-methods/modern-roman.rb
@@ -1,18 +1,25 @@
-def romanise number
+class Integer
+  def romanise
 
-  key = { "M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90,
-    "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" => 4 }
+    number = self
 
-  answer = ""
+    key = { "M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90,
+      "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" => 4 }
 
-  key.each do |roman, amount|
-    times = number / amount
-    answer += roman*times
-    number = number - times*amount
+    answer = ""
+
+    key.each do |roman, amount|
+      times = number / amount
+      answer += roman*times
+      number = number - times*amount
+    end
+
+    puts answer += "I"*number
+
   end
-
-  puts answer += "I"*number
-
 end
 
-romanise(2017)
+# Yes, we're using the British English spelling with an 's' not 'z'
+# because it's just objectively better, okay?
+
+2017.romanise
