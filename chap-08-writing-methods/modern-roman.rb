c95c76621e802566b292df848298c27d6c2b40f3
Timothy Robinson <timmyrobby@gmail.com>
2017-03-03 14:34:48 +0000

    Refactored further using hash

--- chap-08-writing-methods/modern-roman.rb
+++ chap-08-writing-methods/modern-roman.rb
@@ -1,32 +1,17 @@
 def romanise number
 
-  h = { "M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10 }
+  key = { "M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90,
+    "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" => 4 }
 
   answer = ""
 
-  h.each do |roman, amount|
+  key.each do |roman, amount|
     times = number / amount
     answer += roman*times
     number = number - times*amount
   end
 
-  # For fives, or 'V'
-  if number % 5 == 4 && number != 4
-    answer += "IX"
-    number = number - 9
-  else
-    times = number / 5
-    answer += "V"*times
-    number = number - times*5
-  end
-
-  if (number % 4 == 0) && (number != 0)
-    answer += "IV"
-  else
-    answer += "I"*number
-  end
-
-  puts answer
+  puts answer += "I"*number
 
 end
 
