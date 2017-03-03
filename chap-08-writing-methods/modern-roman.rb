e299b918640c6642fe2651063864a09716d11910
Timothy Robinson <timmyrobby@gmail.com>
2017-03-03 14:28:47 +0000

    Refactored using a hash

--- chap-08-writing-methods/modern-roman.rb
+++ chap-08-writing-methods/modern-roman.rb
@@ -1,49 +1,14 @@
 def romanise number
 
-  # For thousands, or 'M'
-  times = number / 1000
-  answer = "M"*times
-  number = number - times*1000
+  h = { "M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10 }
 
-  # For nine-hundreds, or 'CM'
-  times = number / 900
-  answer += "CM"*times
-  number = number - times*900
+  answer = ""
 
-  # For five-hundreds, or 'D'
-  times = number / 500
-  answer += "D"*times
-  number = number - times*500
-
-  # For four-hundreds, or 'CD'
-  times = number / 400
-  answer += "CD"*times
-  number = number - times*400
-
-  # For hundreds, or 'C'
-  times = number / 100
-  answer += "C"*times
-  number = number - times*100
-
-  # For nineties, or 'XC'
-  times = number / 90
-  answer += "XC"*times
-  number = number - times*90
-
-  # For fifties, or 'L'
-  times = number / 50
-  answer += "L"*times
-  number = number - times*50
-
-  # For tens, or 'X', and 'XL' for forties
-  times = number / 10
-  if times == 4
-    answer += "XL"
-  else
-    answer += "X"*times
+  h.each do |roman, amount|
+    times = number / amount
+    answer += roman*times
+    number = number - times*amount
   end
-
-  number = number - times*10
 
   # For fives, or 'V'
   if number % 5 == 4 && number != 4
@@ -64,3 +29,5 @@ def romanise number
   puts answer
 
 end
+
+romanise(2017)
